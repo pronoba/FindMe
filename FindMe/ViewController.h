56b85e09366ffa5dd7539da7353a62a4f0134b1e
Pronob Ashwin <pronob@whizti.com>
2015-02-09 16:50:55 -0800

    Initial Commit

---
+++ FindMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FindMe
+//
+//  Created by Pronob Ashwin on 2/9/15.
+//  Copyright (c) 2015 Pronob Ashwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FindMe/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FindMe
+//
+//  Created by Pronob Ashwin on 2/9/15.
+//  Copyright (c) 2015 Pronob Ashwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
